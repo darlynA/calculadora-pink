e921ddd5e40f59c0fd79ed31ea9b06887927b938
Darlyn.A <prueba@gmail.com>
2023-07-31 21:13:31 -0400

    App calculadora pink

---
+++ script.js
@@ -0,0 +1,30 @@
+let displayValue = '';
+
+function appendValue(value) {
+    displayValue += value;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+}
+
+function deleteLast() {
+    displayValue = displayValue.slice(0, -1);
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+    }
+}
+
+function updateDisplay() {
+    document.getElementById('display').value = displayValue;
+}
